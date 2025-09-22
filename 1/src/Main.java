8a8e7378c47b7d3cb2823b6c43863d6b379801cc
carlos sanchez criado <carlosanchez0897@gmail.com>
2025-09-22 12:28:19 +0200

    .

---
+++ 1/src/Main.java
@@ -0,0 +1,50 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+
+        //EJERCICIO 1//
+
+    String nombre = "Pepe";
+    long nivel = 22;
+    int vida = 15;
+    System.out.println( "El jugador es: " + nombre + "\n , es nivel " + nivel + "\n y tiene " + vida + " puntos de vida");
+
+    //EJERCICIO 2//
+
+    long Pikachu = 2300;
+    long Charmander = 1300;
+    long daño = Pikachu - Charmander;
+    System.out.println( "Pikachu le hace " + daño + " puntos de daño a Charmander");
+
+    //EJERCICIO 3//
+
+    long dinero = 23000;
+    long skin = 2300;
+    long total = dinero - skin;
+    System.out.println("Después de comprar la skin, te quedan " + total + " V-Bucks");
+
+    //EJERCICIO 4//
+
+     double campeon = 0.634;
+     double buff = 0.200;
+     double velocidadTotal = campeon + buff;
+     System.out.println("La velocidad final de Jinx es " + velocidadTotal);
+
+     //EJERCICIO 5//
+
+        long MC = 2300;
+        long VB = MC * 5;
+        long RP = VB * 2;
+                System.out.println(MC  + " Monedas de Mincraft equivalen a " + VB + " V-Bucks y a " + RP + " RP del LOL");
+
+
+
+
+
+
+
+
+
+        }
+    }
